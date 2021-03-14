3ecf6c2ad5f2c98d2d38b0ce4f0fdfe99f2b59b1
Den575 <deniskutchenko008@gmail.com>
2021-03-14 12:28:33 +0100

    add project

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="app">
+    <button class="btn">check</button>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+@import '~materialize-css/dist/css/materialize.min.css';
+@import 'assets/index.css'
+</style>
