ce918130b925af25473fddf1c35c7513cf8b111c
Den575 <deniskutchenko008@gmail.com>
2021-03-15 18:25:58 +0100

    add views

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# vue-youtube
+# vue-project
 
 ## Project setup
 ```
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import Categories from '../views/Categories.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +11,19 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/login',
+    name: 'login',
+    meta: {layout: 'empty'},
+    component: Login
+  }
+  ,
+  {
+    path: '/categories',
+    name: 'categories',
+    meta: {layout: 'main'},
+    component: Categories
   }
 ]
 
