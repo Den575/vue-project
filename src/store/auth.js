129941b9155ec75bacfd33530777b4bedcbede90
Den575 <deniskutchenko008@gmail.com>
2021-03-19 21:07:20 +0100

    update navbar

--- src/store/auth.js
+++ src/store/auth.js
@@ -31,7 +31,8 @@ export default {
         const user = firebase.auth().currentUser
         return user ? user.uid : null
     },
-    async logout() {
+    async logout({commit}) {
         await firebase.auth().signOut()
+        commit('clearInfo')
     }
 }}
