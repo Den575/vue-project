efb1ee4e7dc25da5fe2a5729536015ade9b89568
Den575 <deniskutchenko008@gmail.com>
2021-03-30 18:12:09 +0200

    update profile

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,18 +1,21 @@
 <template>
     <div>
   <div class="page-title">
-    <h3>Профиль</h3>
+    <h3>Konto</h3>
   </div>
 
-  <form class="form">
+  <form class="form" @submit.prevent="submitHandler">
     <div class="input-field">
       <input
           id="description"
           type="text"
+          v-model="name"
+          :class="{invalid: $v.name.$dirty && !$v.name.required}"
       >
       <label for="description">Имя</label>
-      <span
-            class="helper-text invalid">name</span>
+      <small class="helper-text invalid"
+      v-if="$v.name.$dirty && !$v.name.required"
+      >Please enter name</small>
     </div>
 
     <button class="btn waves-effect waves-light" type="submit">
@@ -22,3 +25,45 @@
   </form>
 </div>
 </template>
+
+<script>
+/*global M*/
+import {mapGetters, mapActions} from 'vuex'
+import {required} from 'vuelidate/lib/validators'
+
+export default {
+  data: () => ({
+     name: ''
+  }),
+  validations:{
+    name: {required}
+  },
+  mounted() {
+    this.name = this.info.name
+    setTimeout(() => {
+      M.updateTextFields()
+    }) 
+  },
+  computed: {
+    ...mapGetters(['info'])
+  },
+  methods: {
+    ...mapActions(['updateInfo']),
+    async submitHandler() {
+      if (this.$v.$invalid) {
+        this.$v.$touch()
+        return
+      }
+
+    try{
+      await this.updateInfo({
+        name: this.name
+      })
+    }
+    catch (e){
+      console.log(e);
+    }
+    }
+  }
+}
+</script>
