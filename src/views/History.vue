77ce2bbb97c04d28c7fbd62bfe84358fb69340e3
Amadeusz Rogowski <amadeusz.rogowski@gmail.com>
2021-04-10 15:13:47 +0200

    render new chart

--- src/views/History.vue
+++ src/views/History.vue
@@ -1,41 +1,41 @@
 <template>
-    <div>
-  <div class="page-title">
-    <h3>Historia</h3>
-  </div>
+  <div>
+    <div class="page-title">
+      <h3>Historia</h3>
+    </div>
 
-  <div class="history-chart">
-    <canvas ref="canvas"></canvas>
-  </div>
+    <div class="history-chart">
+      <canvas ref="canvas"></canvas>
+    </div>
 
-  <loader v-if="loading"/>
+    <loader v-if="loading"/>
 
- <p class="center" v-else-if="!records.length">
+    <p class="center" v-else-if="!records.length">
       Jeszcze nic nie ma...
       <router-link to="/record">Dodaj nowy</router-link>
     </p>
-  <section v-else>
-    <HistoryTable :records="items"/>
-    <paginate :page-count="pageCount"
-  :click-handler="pageChangeHandler"
-  :prev-text="'Poprzednia strona'"
-  :next-text="'Kolejna strona'"
-  :container-class="'pagination'"
-  :page-class="'waves-effect'"
-  v-model="page"/>
-  </section>
-</div>
+    <section v-else>
+      <HistoryTable :records="items"/>
+      <paginate :page-count="pageCount"
+                :click-handler="pageChangeHandler"
+                :prev-text="'Poprzednia strona'"
+                :next-text="'Kolejna strona'"
+                :container-class="'pagination'"
+                :page-class="'waves-effect'"
+                v-model="page"/>
+    </section>
+  </div>
 </template>
 
 <script>
 import HistoryTable from '../components/HistoryTable'
-import paginationMixin from '../mixsins/pagination.mixin' 
-import {Pie} from 'vue-chartjs'
+import paginationMixin from '../mixsins/pagination.mixin'
+import {Bar} from 'vue-chartjs'
 
 export default {
   name: 'history',
   mixins: [paginationMixin],
-  extends: Pie,
+  extends: Bar,
   data: () => ({
     loading: true,
     records: []
@@ -43,52 +43,55 @@ export default {
   async mounted() {
     this.records = await this.$store.dispatch('fetchRecords')
     const categories = await this.$store.dispatch('fetchCategories')
-    
+
     this.setup(categories)
-    
+
     this.renderChart({
-        labels: categories.map(c=> c.title),
-        datasets: [{
-            label: 'Przychody i wydatki',
-            data: categories.map( c=> {
-              return this.records.reduce((total, r) => {
-                if(r.categoryId === c.id && r.type === 'outcome'){
-                  total += +r.amount
-                }
-                return total
-              },0)
-            }),
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
+      labels: categories.map(c => c.title),
+      datasets: [
+        {
+          label: 'Wydatki',
+          data: categories.map(c => {
+            return this.records.reduce((total, r) => {
+              if (r.categoryId === c.id && r.type === 'outcome') {
+                total += +r.amount
+              }
+              return total
+            }, 0)
+          }),
+          borderColor: 'rgba(255, 99, 132, 1)',
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          borderWidth: 1,
+        },
+        {
+          label: 'Przychody',
+          data: categories.map(c => {
+            return this.records.reduce((total, r) => {
+              if (r.categoryId === c.id && r.type === 'income') {
+                total += +r.amount
+              }
+              return total
+            }, 0)
+          }),
+          borderColor: 'rgba(75, 192, 192, 1)',
+          backgroundColor: 'rgba(75, 192, 192, 0.2)',
+          borderWidth: 1
+        }
+      ],
     })
+
     this.loading = false
   },
   methods: {
     setup(categories) {
       this.setupPagination(this.records = this.records.map(record => {
-      return {
-        ...record,
-        categoryName: categories.find(c => c.id === record.categoryId).title,
-        typeClass: record.type === 'income' ? 'green' : 'red',
-        typeText: record.type === 'income' ? 'Przychody' : 'Wydatki'
-      }
-    }))
+        return {
+          ...record,
+          categoryName: categories.find(c => c.id === record.categoryId).title,
+          typeClass: record.type === 'income' ? 'green' : 'red',
+          typeText: record.type === 'income' ? 'Przychody' : 'Wydatki'
+        }
+      }))
     }
   },
   components: {
